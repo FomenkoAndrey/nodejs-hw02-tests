c6b68aad9ef68799e6c406dc4c66ea8b08d2c6d4
Alecto <fomenkoandrey1978@gmail.com>
2024-04-21 11:41:31 +0300

    upd env

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 const config = {
   coverageProvider: 'v8',
-  testEnvironment: 'jsdom', // або 'jsdom' для веб-проектів
+  testEnvironment: 'node',
   testMatch: ['**/*.test.js'], // патерн для знаходження тестових файлів
   setupFilesAfterEnv: ['./jest.setup.js'] // додано шлях до вашого файлу налаштувань
 }
